5e7c5937f6c65330e98bfddb3c2d1f730e426c6f
Hitenine <1751050890@qq.com>
2021-02-09 21:39:23 +0800

    feat: image crud and yml

---
+++ src/main/java/com/hitenine/blog/service/impl/BaseService.java
@@ -0,0 +1,7 @@
+package com.hitenine.blog.service.impl;/**
+ * @author Hitenine
+ * @date 2021/2/9 20:38
+ * @version 1.0
+ */
+public class BaseService {
+}
---
+++ src/main/java/com/hitenine/blog/utils/LocalDateTimeUtils.java
@@ -0,0 +1,7 @@
+package com.hitenine.blog.utils;/**
+ * @author Hitenine
+ * @date 2021/2/9 17:48
+ * @version 1.0
+ */
+public class LocalDateTimeUtils {
+}
